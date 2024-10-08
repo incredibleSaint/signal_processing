651bb0c70790a0ebb209c85b87b0525be9269eea
incredibleSaint <senior.dryga2016@yandex.ru>
2024-10-08 18:24:55 +0300

    [r] qpsk visualization [+] module dsp_lib

---
+++ dsp_lib/__init__.py
@@ -0,0 +1,5 @@
+from .dsp_module import sum_double_val, read_signal, make_cmplx_sig, gen_rrc_filter_impulse_response, gen_rc_filter_impulse_response, filter_signal
+from .dsp_module import calculate_spectrum, plot_spectrum, plot_signal, plot_iq
+from .dsp_module import add_freq, add_phase
+from .dsp_module import resampling_lagrange_step_ver, resampling_lagrange
+from .dsp_module import gen_qpsk_symbols
---
+++ dsp_lib/dsp_module.py
@@ -0,0 +1,245 @@
+import os
+from array import array
+import numpy as np
+from commpy.filters import rcosfilter
+from commpy.filters import rrcosfilter
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy.signal import butter, filtfilt
+#import ipympl
+#%matplotlib widget
+#%matplotlib inline
+
+
+def sum_double_val(a, b):
+    return a + b
+
+def read_signal(file_name, data_type, data_num=0, shift=0, separator=''):
+    # Support of data_type: int16, float32, float64 
+    # Possible separator: '\n' every num on new line, '' - nothing between samples
+    
+    file_stats = os.stat(file_name)
+    file_size = file_stats.st_size
+    #print(file_size)
+    if data_num == 0:
+        if data_type == 'float32':
+            data_num = int(file_size / 4)
+        if data_type == 'int16':
+            data_num = int(file_size / 2)
+        if data_type == 'float64':
+            data_num = int(file_size / 8)
+
+    if data_type == 'float32':
+        t = np.float32
+    if data_type == 'int16':
+        t = np.int16
+    if data_type == 'float64':
+        t = np.float64
+    
+    with open(file_name, 'rb') as file:
+    # Read the binary data and convert it to a NumPy array
+        data = np.fromfile(file, dtype=t, count=data_num, offset=shift, sep=separator)
+    
+    return data
+
+def make_cmplx_sig(iq_data):
+    i = np.array(iq_data[:: 2])
+    i = i.reshape((len(i), 1))
+    q = np.array(iq_data[1:: 2])
+    q = q.reshape((len(q), 1))
+    print(i[:10])
+    sig = np.empty((len(i), 1), complex)
+    sig.real = i
+    sig.imag = q
+    sig_flatten = sig.flatten()
+    return sig_flatten
+
+def calculate_spectrum(signal):
+    yf = np.fft.fft(signal)
+    yf = np.abs(yf.reshape((len(yf), 1))) / len(signal)
+    return yf
+
+def plot_spectrum(signal, figure_counter, title="Amplitude spectrum"):
+    spectrum = np.roll(calculate_spectrum(signal), len(signal) // 2)
+    yf = 20 * np.log10(spectrum)
+
+    plt.figure(figure_counter)
+    plt.cla()
+    x_data = np.arange(-len(spectrum) // 2 + 1, len(spectrum) // 2 + 1)
+    x_data = x_data / (len(spectrum) / 2)
+    plt.plot(x_data, yf)
+    plt.grid(1)
+    plt.xlabel('Frequency (semicircles)')
+    plt.ylabel('Magnitude (dB)')
+    plt.title(title)
+    plt.show()
+    return yf
+
+def plot_signal(signal, figure_counter, title="Signal Power"):
+    plt.figure(figure_counter)
+    plt.cla()
+    x_data = np.arange(0, len(signal))
+    plt.plot(x_data, signal)
+    plt.grid(1)
+    plt.xlabel('time (samples)')
+    plt.ylabel('Signal Power')
+    plt.title(title)
+    plt.show()
+
+def add_freq(sig, f0_to_fsig):
+    n = np.arange(0, len(sig), 1)
+    mixer = np.exp(1j * np.pi * f0_to_fsig * n)
+    return sig * mixer
+
+def add_phase(sig, phase):
+# phase = [-1, 1] [semicircles]
+    sig *= np.exp(1j * np.pi * phase)
+    return sig
+
+
+def gen_rrc_filter_impulse_response(n, f_sampl, t_symb, roll_off=0.24):
+    assert (n % 2 == 1), "RRC filter length should be odd!"
+    n += 1 # because first sample will be not used
+    s_psf = rrcosfilter(n * f_sampl, roll_off, Ts=t_symb, Fs=f_sampl)[1][1:]
+    return s_psf
+#%%
+def gen_rc_filter_impulse_response(n, f_sampl, t_symb, roll_off=0.24):
+    assert (n % 2 == 1), "RC filter length should be odd!"
+    n += 1 # because first sample will be not used
+    s_psf = rcosfilter(n * f_sampl, roll_off, Ts=t_symb, Fs=f_sampl)[1][1:]
+    return s_psf
+
+def resampling_lagrange_step_ver(s, step, x0):
+    """
+    % y = resample_lagrange(s, p, q, x0)
+    % Digital resampling by polynomial Lagrange interpolation.
+    % Function changes input signal s samplerate to p/q times and adds fractional
+    % delay.
+    %
+    % Input parameters
+    %  s   - input signal vector [N x 1];
+    %  p   - p paramter of samplarate conversion
+    %  q   - q paramter of samplarate conversion
+    %  x0  - fractional delay
+    %
+    % Ouptut parameters
+    %  y   - Resampled signal
+    %
+    % Author: Sergey Bakhurin (dsplib.org)
+    """
+    #if (p > 1):
+    #    if (q == 1):
+    #        y = np.zeros(int(float((len(s) - 1) * p) / float(q)) + 1, dtype=np.csingle)
+    #    else:
+    #        y = np.zeros(int(float(len(s) * p) / float(q)), dtype=np.csingle)
+    #else:
+    #    y = np.zeros(int(float(len(s) * p) / float(q)), dtype=np.csingle)
+    if (step > 1):
+        y = np.zeros(int((len(s) -1) / step) + 1, dtype=np.csingle)
+    else:
+        y = np.zeros(int(len(s) / step), dtype=np.csingle) 
+    
+    t = np.zeros(len(y))
+    s = np.concatenate((np.array([0., 0.]), s, np.array([0., 0.])))
+
+    print(f"len(y) = {len(y)}, len(t) = {len(t)}, len(s) = {len(s)}")
+
+    for k in range(len(y)):
+        #x = k * q / p - x0
+        x = k * step - x0
+        t[k] = x
+        n = int(np.floor(x)) + 4
+        d = np.floor(x) + 1 - x
+        #print(f"x = {x}, n = {n}, d = {d}")
+        a0 = s[n - 1]
+        a3 = 1 / 6 * (s[n] - s[n - 3]) + 0.5 * (s[n - 2] - s[n - 1])
+        a1 = 0.5 * (s[n] - s[n - 2]) - a3
+        a2 = s[n] - s[n - 1] - a3 - a1
+        #print(f"a0 = {a0} a1 = {a1} a2 = {a2} a3 = {a3}")
+        y[k] = a0 - a1 * d + a2 * d ** 2 - a3 * d ** 3
+
+    #print(t)
+    return y
+
+def resampling_lagrange(s, p, q, x0):
+    """
+    % y = resample_lagrange(s, p, q, x0)
+    % Digital resampling by polynomial Lagrange interpolation.
+    % Function changes input signal s samplerate to p/q times and adds fractional
+    % delay.
+    %
+    % Input parameters
+    %  s   - input signal vector [N x 1];
+    %  p   - p paramter of samplarate conversion
+    %  q   - q paramter of samplarate conversion
+    %  x0  - fractional delay
+    %
+    % Ouptut parameters
+    %  y   - Resampled signal
+    %
+    % Author: Sergey Bakhurin (dsplib.org)
+    """
+    if (p > 1):
+        if (q == 1):
+            y = np.zeros(int(float((len(s) - 1) * p) / float(q)) + 1, dtype=np.csingle)
+        else:
+            y = np.zeros(int(float(len(s) * p) / float(q)), dtype=np.csingle)
+    else:
+        y = np.zeros(int(float(len(s) * p) / float(q)), dtype=np.csingle)
+
+    t = np.zeros(len(y))
+    s = np.concatenate((np.array([0., 0.]), s, np.array([0., 0.])))
+
+    for k in range(len(y)):
+        x = k * q / p - x0
+        t[k] = x
+        n = int(np.floor(x)) + 4
+        d = np.floor(x) + 1 - x
+        a0 = s[n - 1]
+        a3 = 1 / 6 * (s[n] - s[n - 3]) + 0.5 * (s[n - 2] - s[n - 1])
+        a1 = 0.5 * (s[n] - s[n - 2]) - a3
+        a2 = s[n] - s[n - 1] - a3 - a1
+
+        y[k] = a0 - a1 * d + a2 * d ** 2 - a3 * d ** 3
+
+    return y
+
+def filter_signal(sig, normal_cutoff):
+    order = 2
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    sig_filtered = filtfilt(b, a, sig)
+    return sig_filtered
+
+def gen_qpsk_symbols(n_symb, fs):
+    # Create IQ of QPSK
+    # complex array: [(1 + 1j, 1 -1j, ..., -1 -1j)]
+    data = np.random.randint(0, 4, n_symb)
+    #print(len(data))
+    #x_tr = x.transpose()
+    y = [-1 - 1j, -1 + 1j, 1 - 1j, 1 + 1j]
+    iq = [y[val] for val in data]
+    iq_ups = np.zeros(n_symb * fs, dtype=complex)
+    iq_ups[:len(iq) * fs:fs] = iq
+    return iq_ups
+
+
+def plot_iq( delay: float, fsymb_to_fsampl: float, phase: float, f0: float):
+    print(f"fract omega = {f0}, fsymb_to_fsampl = {fsymb_to_fsampl}, phase = {phase}")
+    matplotlib.pyplot.close()
+
+    signal_iq = qpsk_filtered
+    
+    mixed_sig = add_freq(signal_iq, f0)
+    sig_default = add_phase(mixed_sig, phase)
+
+    p = 1
+    q = round(fsymb_to_fsampl) #todo -> remove round()
+    x0 = delay
+    print(f"p = {p}, q = {q}, x = {x0}")
+    res_sig = resampling_lagrange_step_ver(sig_default, fsymb_to_fsampl, x0)
+    #res_sig = resampling_lagrange(sig_default, p, q, x0)
+    plt.close()
+    plt.figure()
+    plt.plot(res_sig.real, res_sig.imag, '.')
+    plt.grid()
+    plt.show()
