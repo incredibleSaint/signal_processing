7522f16969c237eba3e0ed56315241fc0a248d4e
slava_dryga <senior.dryga2016@yandex.ru>
2024-10-14 10:34:30 +0300

    [f] partional convolution [-] dsp module: not used func

--- dsp_lib/__init__.py
+++ dsp_lib/__init__.py
@@ -1,4 +1,4 @@
-from .dsp_module import sum_double_val, read_signal, make_cmplx_sig, save_cmplx_sig
+from .dsp_module import read_signal, make_cmplx_sig, save_cmplx_sig
 from .dsp_module import gen_rrc_filter_impulse_response, gen_rc_filter_impulse_response, filter_signal
 from .dsp_module import calculate_spectrum, plot_spectrum, plot_signal, plot_iq
 from .dsp_module import add_freq, add_phase
